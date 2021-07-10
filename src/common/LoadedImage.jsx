f91e77763bb20d555876fa6e180068814f5e77eb
Chris Bouchard <chris@upliftinglemma.net>
2021-07-10 04:58:56 -0400

    Refactor LoadedImage to use a reducer for state

--- src/common/LoadedImage.jsx
+++ src/common/LoadedImage.jsx
@@ -1,34 +1,79 @@
-import { useState } from "react";
-import { Dimmer, Image, Loader } from "semantic-ui-react";
+import { useLayoutEffect, useReducer } from "react";
+import { Dimmer, Image, Label, Loader } from "semantic-ui-react";
+
+const initialState = {
+    loading: false,
+    loaded: false,
+    errored: false,
+    complete: false,
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "reset":
+            return {
+                ...state,
+                loading: action.src != null,
+                loaded: false,
+                errored: false,
+                complete: false,
+            };
+        case "load":
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                complete: true,
+            };
+        case "error":
+            return {
+                ...state,
+                loading: false,
+                errored: true,
+                complete: true,
+            };
+        default:
+            throw new Error(`Unknown action type: ${action.type}`);
+    }
+}
 
 export function LoadedImage({
+    alt,
     loading = false,
+    placeholderAlt,
     placeholderSrc,
     src,
     ...imageProps
 }) {
-    const [imageLoaded, setImageLoaded] = useState(false);
-    const [imageErrored, setImageErrored] = useState(false);
+    const [imageState, dispatch] = useReducer(reducer, initialState);
 
-    const dimmerActive = loading || (!imageLoaded && !imageErrored);
+    // Reset state when the image source changes.
+    useLayoutEffect(() => {
+        dispatch({ type: "reset", src });
+    }, [src]);
 
-    const displayImage = !loading && imageLoaded;
-    const displayPlaceholder = !displayImage;
+    const dimmerActive =
+        (loading && !imageState.complete) || imageState.loading;
 
     return (
         <Image {...imageProps}>
             <Dimmer inverted active={dimmerActive}>
                 <Loader />
             </Dimmer>
+            {imageState.errored ? (
+                <Label color="red" corner="right" icon="warning sign" />
+            ) : null}
             <img
                 src={src}
-                style={{ display: displayImage ? "block" : "none" }}
-                onLoad={() => setImageLoaded(true)}
-                onError={() => setImageErrored(true)}
+                alt={alt}
+                style={{ display: imageState.loaded ? "block" : "none" }}
+                onLoad={() => dispatch({ type: "load" })}
+                onError={() => dispatch({ type: "error" })}
             />
             <img
                 src={placeholderSrc}
-                style={{ display: displayPlaceholder ? "block" : "none" }}
+                alt={placeholderAlt ?? <>Loading&hellip;</>}
+                style={{ display: !imageState.loaded ? "block" : "none" }}
             />
         </Image>
     );
