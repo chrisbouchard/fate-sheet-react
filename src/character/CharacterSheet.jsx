2c26944c9b032917d8683b8f2240784bd73a3b0e
Chris Bouchard <chris@upliftinglemma.net>
2021-07-10 22:30:01 -0400

    Add character image and space for world to sheet

--- src/character/CharacterSheet.jsx
+++ src/character/CharacterSheet.jsx
@@ -1,17 +1,44 @@
-import { Header, Segment } from "semantic-ui-react";
+import { Grid, Header, Segment } from "semantic-ui-react";
+
+import { LoadedImage } from "../common/LoadedImage";
+
+import placeholderImageUri from "./placeholder.svg";
 
 export function CharacterSheet({ character, loading }) {
     return (
-        <Segment stacked>
-            <Header size="huge" dividing>
-                {character?.name ??
-                    (loading ? <>Loading&hellip;</> : "Unknown Character")}
-                <Header.Subheader>
-                    {character?.aspects
-                        ? character.aspects.data[0]?.name
-                        : null}
-                </Header.Subheader>
-            </Header>
-        </Segment>
+        <>
+            <Grid as={Segment} stacked divided="vertically">
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <LoadedImage
+                            placeholderSrc={placeholderImageUri}
+                            loading={loading}
+                            bordered
+                        />
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Header size="huge">
+                            {character?.name ??
+                                (loading ? (
+                                    <>Loading&hellip;</>
+                                ) : (
+                                    "Unknown Character"
+                                ))}
+                            <Header.Subheader>
+                                {character?.aspects
+                                    ? character.aspects.data[0]?.name
+                                    : null}
+                            </Header.Subheader>
+                        </Header>
+                    </Grid.Column>
+                    <Grid.Column width={4}>World</Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={16}>
+                        <p>Hello World!</p>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </>
     );
 }
