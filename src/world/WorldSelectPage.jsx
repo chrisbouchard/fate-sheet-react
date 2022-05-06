5d0c2e11d4da5208a3073ac28291582deba3bd8a
Chris Bouchard <chris@upliftinglemma.net>
2022-05-05 22:18:47 -0400

    Rewrite WorldSelectPage to use SWR directly
    The goal is to deprecate and eventually remove the generic <Fetch> component in
    favor of using SWR directly. This will let us use the full feature set of SWR in
    our components.

--- src/world/WorldSelectPage.jsx
+++ src/world/WorldSelectPage.jsx
@@ -1,15 +1,23 @@
+import useSWR from "swr";
 import { Suspense } from "react";
 
-import { Fetch } from "../api/Fetch";
+import { useApi } from "../api/Api";
 
 import { WorldList } from "./WorldList";
+
+function FetchWorlds({ children: render }) {
+    const { apiFetch } = useApi();
+    const { data: { data: worlds } } = useSWR("worlds", apiFetch, { suspense: true });
+
+    return render(worlds);
+}
 
 export function WorldSelectPage() {
     return (
         <Suspense fallback={<WorldList loading />}>
-            <Fetch resource="worlds">
-                {({ data: worlds }) => <WorldList worlds={worlds} />}
-            </Fetch>
+            <FetchWorlds>
+                {(worlds) => <WorldList worlds={worlds} />}
+            </FetchWorlds>
         </Suspense>
     );
 }
